b40156b4100287f889c8c76112ae4b4b8d7265bd
NotesLink <domainwebsitedns@gmail.com>
2023-06-06 22:46:49 +0530

    serviceworker

--- fetch-ls.js
+++ fetch-ls.js
@@ -7,7 +7,7 @@ fetchList = async () => {
         let link = document.createElement('a');
         let div = document.createElement('div');
         let img = document.createElement('img')
-        link.setAttribute('href', `https://github.com/NotesLink/c10/raw/main/${json[i].path}`);
+        link.setAttribute('href', `javascript:dl("https://noteslink.github.io/c10/${json[i].path}")`);
         div.innerHTML = json[i].name;
         img.setAttribute('src', '/file-icon.png');
         link.appendChild(img);
@@ -15,3 +15,39 @@ fetchList = async () => {
         list.appendChild(link);
     }
 }
+
+const registerServiceWorker = async () => {
+    if ("serviceWorker" in navigator) {
+        try {
+            const registration = await navigator.serviceWorker.register("/sw.js", {
+                scope: "/",
+            });
+            if (registration.installing) {
+                console.log("Service worker installing");
+            } else if (registration.waiting) {
+                console.log("Service worker installed");
+            } else if (registration.active) {
+                console.log("Service worker active");
+            }
+        } catch (error) {
+            console.error(`Registration failed with ${error}`);
+        }
+    }
+};
+registerServiceWorker();
+
+function addManifest() {
+    let l = document.createElement('link');
+    l.setAttribute("rel", "manifest");
+    l.setAttribute("href", "/.webmanifest");
+    document.head.appendChild(l);
+}
+
+dl = (url) => {
+    fetch(url).then(res => res.blob()).then(blob => {
+        var f = window.URL.createObjectURL(blob);
+        window.location.assign(f);
+    });
+}
+
+load = () => { fetchList(); addManifest(); }
---
+++ sw.js
@@ -0,0 +1,56 @@
+refreshWorker = async () => {
+    fetch('/res.json').then((res) => {
+        res.json().then((json) => {
+            caches.open("2").then((cache) => {
+                for (let m in json.ls) {
+                    console.log(json.ls[m]);
+                    cache.delete(json.ls[m]);
+                    cache.add(json.ls[m]);
+                }
+                for (let i in json.gh) {
+                    fetch(json.gh[i]).then((res) => {
+                        cache.delete(json.gh[i]);
+                        cache.add(json.gh[i]);
+                        console.log(json.gh[i]);
+                        res.json().then((k) => {
+                            for (let j in k) {
+                                console.log(k[j]);
+                                cache.delete(`https://noteslink.github.io/c10/${k[j].path}`);
+                                cache.add(`https://noteslink.github.io/c10/${k[j].path}`);
+                            }
+                        });
+                    });
+                }
+            });
+        }).catch((err) => {
+            console.log(err);
+        });
+    });
+}
+
+self.addEventListener("install", () => {
+    refreshWorker();
+    self.skipWaiting();
+});
+
+const cacheFetch = async (req) => {
+    let path;
+    if (new URL(req.url).host !== new URL(self.origin).host) {
+        path = req.url;
+    } else {
+        path = new URL(req.url).pathname;
+    }
+    let check = await caches.match(path);
+    console.log(path);
+    if (check) {
+        console.log("served " + req.url + " from cache");
+        return await caches.match(path);
+    } else {
+        console.log("fetching " + req.url);
+        return await fetch(req);
+    }
+};
+
+self.addEventListener("fetch", (e) => {
+    e.respondWith(cacheFetch(e.request));
+});
